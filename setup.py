84a16ece6a34c921c0740a1f9552354522f6a7fc
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-01-02 13:11:13 +0300

    Updated e-mail and url.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='NlpToolkit-NamedEntityRecognition',
     version='1.0.5',
     packages=['NamedEntityRecognition'],
-    url='https://github.com/olcaytaner/TurkishNamedEntityRecognition-Py',
+    url='https://github.com/StarlangSoftware/TurkishNamedEntityRecognition-Py',
     license='',
     author='olcaytaner',
-    author_email='olcaytaner@isikun.edu.tr',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
     description='NER Corpus Processing Library',
     install_requires=['NlpToolkit-Corpus']
 )
