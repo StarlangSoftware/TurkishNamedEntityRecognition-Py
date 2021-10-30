8624a7f1098bf0b9960daf6cb4dd39188414308c
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:51:40 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-NamedEntityRecognition',
-    version='1.0.7',
+    version='1.0.8',
     packages=['NamedEntityRecognition'],
     url='https://github.com/StarlangSoftware/TurkishNamedEntityRecognition-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='NER Corpus Processing Library',
-    install_requires=['NlpToolkit-Corpus']
+    install_requires=['NlpToolkit-Corpus'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
